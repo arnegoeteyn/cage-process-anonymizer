18143c84aea4644781382aeda9827c1b3240f83a
Frederic Lebeau <frederic@datavillage.me>
2023-03-14 20:20:41 +0100

    first algo ano

--- process.py
+++ process.py
@@ -5,6 +5,34 @@ import os
 import pandas as pd
 from dv_utils import default_settings, Client
 
+import csv
+import pprint
+import collections
+from typing import List, Iterable, Optional, Union, Dict
+
+from presidio_analyzer import AnalyzerEngine, BatchAnalyzerEngine, RecognizerResult, DictAnalyzerResult
+from presidio_anonymizer import AnonymizerEngine
+from presidio_anonymizer.entities import EngineResult
+
+"""
+Example implementing a CSV analyzer
+
+This example shows how to use the Presidio Analyzer and Anonymizer
+to detect and anonymize PII in a CSV file.
+It uses the BatchAnalyzerEngine to analyze the CSV file, and the
+BatchAnonymizerEngine to anonymize the requested columns.
+Note that currently BatchAnonymizerEngine is not part of the anonymizer package,
+and is defined in this and the batch_processing notebook.
+https://github.com/microsoft/presidio/blob/main/docs/samples/python/batch_processing.ipynb
+
+Content of csv file:
+id,name,city,comments
+1,John,New York,called him yesterday to confirm he requested to call back in 2 days
+2,Jill,Los Angeles,accepted the offer license number AC432223
+3,Jack,Chicago,need to call him at phone number 212-555-5555
+
+"""
+
 logger = logging.getLogger(__name__)
 
 # let the log go to stdout, as it will be captured by the cage operator
@@ -12,6 +40,85 @@ logging.basicConfig(
     level=default_settings.log_level,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
+
+class CSVAnalyzer(BatchAnalyzerEngine):
+
+    def analyze_csv(
+        self,
+        csv_full_path: str,
+        language: str,
+        keys_to_skip: Optional[List[str]] = None,
+        **kwargs,
+    ) -> Iterable[DictAnalyzerResult]:
+
+        with open(csv_full_path, 'r') as csv_file:
+            csv_list = list(csv.reader(csv_file))
+            csv_dict = {header: list(map(str, values)) for header, *values in zip(*csv_list)}
+            analyzer_results = self.analyze_dict(csv_dict, language, keys_to_skip)
+            return list(analyzer_results)
+
+
+class BatchAnonymizerEngine(AnonymizerEngine):
+    """
+    Class inheriting from the AnonymizerEngine and adding additional functionality 
+    for anonymizing lists or dictionaries.
+    """
+
+    def anonymize_list(
+        self,
+        texts:List[Union[str, bool, int, float]], 
+        recognizer_results_list: List[List[RecognizerResult]], 
+        **kwargs
+    ) -> List[EngineResult]:
+        """
+        Anonymize a list of strings.
+
+        :param texts: List containing the texts to be anonymized (original texts)
+        :param recognizer_results_list: A list of lists of RecognizerResult,
+        the output of the AnalyzerEngine on each text in the list.
+        :param kwargs: Additional kwargs for the `AnonymizerEngine.anonymize` method
+        """
+        return_list = []
+        if not recognizer_results_list:
+            recognizer_results_list = [[] for _ in range(len(texts))]
+        for text, recognizer_results in zip(texts, recognizer_results_list):
+            if type(text) in (str, bool, int, float):
+                res = self.anonymize(text=str(text), analyzer_results=recognizer_results, **kwargs)
+                return_list.append(res.text)
+            else:
+                return_list.append(text)
+
+        return return_list
+
+    def anonymize_dict(self, analyzer_results: Iterable[DictAnalyzerResult], **kwargs) -> Dict[str, str]:
+
+        """
+        Anonymize values in a dictionary.
+
+        :param analyzer_results: Iterator of `DictAnalyzerResult` 
+        containing the output of the AnalyzerEngine.analyze_dict on the input text.
+        :param kwargs: Additional kwargs for the `AnonymizerEngine.anonymize` method
+        """
+
+        return_dict = {}
+        for result in analyzer_results:
+
+            if isinstance(result.value, dict):
+                resp = self.anonymize_dict(analyzer_results = result.recognizer_results, **kwargs)
+                return_dict[result.key] = resp
+
+            elif isinstance(result.value, str):
+                resp = self.anonymize(text=result.value, analyzer_results=result.recognizer_results, **kwargs)
+                return_dict[result.key] = resp.text
+
+            elif isinstance(result.value, collections.abc.Iterable):
+                anonymize_respones = self.anonymize_list(texts=result.value,
+                                                         recognizer_results_list=result.recognizer_results, 
+                                                         **kwargs)
+                return_dict[result.key] = anonymize_respones 
+            else:
+                return_dict[result.key] = result.value
+        return return_dict
 
 # define an event processing function
 def event_processor(evt: dict):
@@ -24,12 +131,9 @@ def event_processor(evt: dict):
         logger.info(f"Processing event {evt}")
 
         evt_type =evt.get("type", "")
-        if(evt_type == "QUOTE"):
-           logger.info(f"use the update quote event processor")
+        if(evt_type == "ANONYMISE"):
+           logger.info(f"use the anonymise event processor")
            update_quote_event_processor(evt)
-        else:
-           logger.info(f"Unhandled message type, use the generic event processor")
-           generic_event_processor(evt)
 
     # pylint: disable=broad-except
     except Exception as err:
@@ -38,75 +142,20 @@ def event_processor(evt: dict):
         logger.info(f"Processed event in {time.time() - start:.{3}f}s")
 
 
-
-def generic_event_processor(evt: dict):
-   client = Client()
-
-   # Use userIds provided in the event, or get all active users for this application
-   user_ids = evt.get("userIds", []) or client.get_users()   
-
-   logger.info(f"Processing {len(user_ids)} users")
-   for user_id in user_ids:
-      try:
-         # retrieve data graph for user
-         # user_data = client.get_data(user_id) or {}
-
-         # for the sake of this example, write some RDF with a dummy 0 count into the user's pod
-         client.write_results(user_id, "inferences", f"<https://datavillage.me/{user_id}> <https://datavillage.me/count> 0" )
-
-      # pylint: disable=broad-except
-      except Exception as err:
-         logger.warning(f"Failed to process user {user_id} : {err}")
-
-
 def update_quote_event_processor(evt: dict):
    client = Client()
 
-   STOCK_XL_PATH = os.environ.get("STOCK_XL_PATH", "")
-   FMP_API_KEY = os.environ.get("FMP_API_KEY", "")
-   if(not STOCK_XL_PATH):
-       raise RuntimeError("STOCK_XL_PATH environment variable is not defined")
-   if(not FMP_API_KEY):
-       raise RuntimeError("FMP_API_KEY environment variable is not defined")
+   PII_CSV_PATH = os.environ.get("PII_CSV_PATH", "")
+   if(not PII_CSV_PATH):
+       raise RuntimeError("PII_CSV_PATH environment variable is not defined")
+   
+   analyzer = CSVAnalyzer()
+   analyzer_results = analyzer.analyze_csv(PII_CSV_PATH,
+                                            language="en")
 
-   # get the stocks to quote from xlsx file
-   stocks = pd.read_excel(STOCK_XL_PATH)
-   stocks = stocks.set_index("symbol")
+   anonymizer = BatchAnonymizerEngine()
+   anonymized_results = anonymizer.anonymize_dict(analyzer_results)
 
-   # get the quotes for the symbol through an API call to FinancialModelingPrep
-   symbols = ",".join(stocks.index)
-   stock_response = requests.get(f"https://financialmodelingprep.com/api/v3/quote-short/{symbols}?apikey={FMP_API_KEY}")
-   stock_quotes = stock_response.json()
-
-   # merge quotes with symbols in a unified dataframe and possibly save it to file
-   stocks = stocks.join(pd.DataFrame(stock_quotes).set_index("symbol") )
-   try:
-       stocks.to_excel("/resources/outputs/stocks.xlsx")
-   except:
-      pass
-
-   # prepare rdf file with the quotes following schema.org onthology
-   rdf_content = "".join([f"""
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Intangible/FinancialQuote> .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/additionalType> <https://schema.org/FinancialProduct> .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/tickerSymbol> "{r["symbol"]}" .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/exchange> "{r["exchange"]}" .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/name> "{r["name"]}" .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/price> "{r["price"]}"^^<http://www.w3.org/2001/XMLSchema#float> .
-<https://www.google.com/finance/quote/{r["exchange"]}:{r["symbol"]}> <https://schema.org/volume> "{r["volume"]}"^^<http://www.w3.org/2001/XMLSchema#float> .
-   """ for r in stocks.reset_index().to_dict("records")])
-   logger.info(f"Generated RDF content:\n{rdf_content}")
-
-   # Use userIds provided in the event, or get all active users for this application
-   user_ids = evt.get("userIds",[]) or client.get_users()
-
-   # Save the stock quotes in the data vault of all the users
-   logger.info(f"Processing {len(user_ids)} users")
-   for user_id in user_ids:
-      try:
-         # for the sake of this example, write some RDF with the number of user statements into the user's pod
-         client.write_results(user_id, "inferences", rdf_content)
-
-      # pylint: disable=broad-except
-      except Exception as err:
-         logger.warning(f"Failed to process user {user_id} : {err}")
+   with open(os.environ.get("PII_CSV_ANONYMIZED_PATH", ""), 'w', newline='') as file:
+        for key in anonymized_results.keys():
+            file.write("%s,%s\n"%(key,anonymized_results[key]))
